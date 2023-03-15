b45545340c96b5ec9575d1eae4a04a5225656088
Adam Topaz <adamtopaz@users.noreply.github.com>
2023-03-15 21:30:03 +0000

    wd

--- src/elan/install.sh
+++ src/elan/install.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -e
 
-su - vscode
-echo "The following should say vscode"
-whoami
-echo "The following should say /home/vscode"
-echo $HOME
-curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh -s -- -y --default-toolchain none
+su ${USERNAME} -c "whoami"
+su vscode -c "whoami"
+su vscode -c "curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh -s -- -y --default-toolchain none"
 
 # TODO: Currently the TOOLCHAIN option is not used.

d2764a62a2561a54dc9ed1606ab210f2944784df
Adam Topaz <adamtopaz@users.noreply.github.com>
2023-03-15 21:37:20 +0000

    fix

--- src/elan/install.sh
+++ src/elan/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-su vscode -c "curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh -s -- -y --default-toolchain ${TOOLCHAIN}"
+su vscode -c "curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh -s -- -y --default-toolchain $TOOLCHAIN"
 
 # TODO: Currently the TOOLCHAIN option is not used.
